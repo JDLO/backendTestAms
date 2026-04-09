2719b7fc7b3dda5d5d82bc1fe526b3ffed603eb9
JDLO <juandavidleono@gmail.com>
2026-04-09 23:11:34 +0200

    first commit

---
+++ backendTestAms/src/main/java/com/jdlotest/backendTestAms/BackendTestAmsApplication.java
@@ -0,0 +1,13 @@
+package com.jdlotest.backendTestAms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestAmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTestAmsApplication.class, args);
+	}
+
+}
