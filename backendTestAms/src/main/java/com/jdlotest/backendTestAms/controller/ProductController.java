6f88aa8fee53be375ccbe0f99b71658949c362b2
JDLO <juandavidleono@gmail.com>
2026-04-10 17:08:06 +0200

    Se sobre escribe el metdo getProdictSimilar

--- backendTestAms/src/main/java/com/jdlotest/backendTestAms/controller/ProductController.java
+++ backendTestAms/src/main/java/com/jdlotest/backendTestAms/controller/ProductController.java
@@ -1,7 +1,28 @@
 package com.jdlotest.backendTestAms.controller;
 
-import com.jdlotest.backendTestAms.api.ProductApi;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Set;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.jdlotest.backendTestAms.api.ProductApi;
+import com.jdlotest.backendTestAms.model.ProductDetail;
+import com.jdlotest.backendTestAms.service.ProductService;
+
+@Controller
 public class ProductController implements ProductApi {
+	private ProductService service;
+	
+	public ProductController(ProductService productService) {
+		this.service = productService;
+	}
+
+	@Override
+	public ResponseEntity<Set<ProductDetail>> getProductSimilar(String productId) {
+		Set<ProductDetail> response = service.getSimilarProducts(productId);
+		return ResponseEntity.ok(response);
+	}	
 
 }
