d4526589d82ce3a249a572ae2ed0eed0518ab7a4
JDLO <juandavidleono@gmail.com>
2026-04-10 18:11:37 +0200

    Se agrega nueva manera para la concurrencia

--- backendTestAms/src/main/java/com/jdlotest/backendTestAms/service/ProductService.java
+++ backendTestAms/src/main/java/com/jdlotest/backendTestAms/service/ProductService.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -16,13 +18,15 @@ import com.jdlotest.backendTestAms.model.ProductDetail;
 
 @Service
 public class ProductService {
-	//@Value("${api.mocks.url}")
 	private static String urlMock = "http://localhost:3001";
 	
 	private final RestClient restClient;
 	
+	private final ExecutorService executor;
+	
 	public ProductService(RestClient.Builder builder) {
 		this.restClient = builder.baseUrl(urlMock).build();
+		this.executor = Executors.newVirtualThreadPerTaskExecutor();
 	}
 	
 	public Set<ProductDetail> getSimilarProducts(String productId){
@@ -35,8 +39,12 @@ public class ProductService {
 			if (similarIds == null||similarIds.isEmpty()) return Set.of();
 						
 			List<CompletableFuture<ProductDetail>> futures = similarIds.stream()
-					.map(id -> CompletableFuture.supplyAsync(()-> fetchProductDetail(id)))
+					.map(id -> CompletableFuture.supplyAsync(()-> fetchProductDetail(id), executor)
+							.orTimeout(3, TimeUnit.SECONDS)
+							.exceptionally(ex -> null))
 					.toList();
+			
+			CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 			
 			return futures.stream()
 					.map(CompletableFuture::join)
